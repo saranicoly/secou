ce8c5e775b3aabe922b259591ecc6ccfaa422f50
Luiz Eduardo Schmalz <102548215+luizschmalz@users.noreply.github.com>
2024-07-09 00:55:16 -0300

    padronizando nome das keys

--- backend/test_elevation.py
+++ backend/test_elevation.py
@@ -2,10 +2,10 @@ import googlemaps
 from elevationAPI import elevation, elevation_along_path  
 import os
 
-API_KEY = os.environ['GCP_API_KEY']
+GCP_KEY = os.environ['GCP_API_KEY']
 
 def test_elevation():
-    client = googlemaps.Client(key=API_KEY)
+    client = googlemaps.Client(key=GCP_KEY)
     locations = [
         (34.052235, -118.243683),  # Los Angeles, CA
         (40.712776, -74.005974),   # New York, NY
