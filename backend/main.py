af32f2f1d416d933e4931443a1ac0d2af0f8aab3
luizschmalz <lefvs@cin.ufpe.br>
2024-07-13 14:52:07 -0300

    mapa implementado

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,25 @@ import os
 
 app = FastAPI()
 
+# Configurar CORS
+origins = [
+    "http://localhost:8100",  # Origem do seu frontend Ionic
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/google-maps-api-key")
+async def get_google_maps_api_key():
+    api_key = os.environ.get("GCP_KEY")
+    if api_key:
+        print(f"GCP_KEY found: {api_key}")
+        return {"apiKey": api_key}
+    else:
+        print("GCP_KEY not found or is None")
+        return {"apiKey": None}
