97a7afbe7ae23595441a5040ceea6f59b20681d1
Sara Lins <sara.lins@luizalabs.com>
2024-08-04 21:13:50 -0300

    feat: add google maps

--- frontend/src/app/tab1/tab1.page.ts
+++ frontend/src/app/tab1/tab1.page.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
   showSearchForm = false;
   saida!: string;
   destino!: string;
   horario!: string;
+  center!: google.maps.LatLngLiteral;
+  zoom: number = 15;
 
   constructor() {}
+
+  ngOnInit() {
+    this.getCurrentLocation();
+  }
+
+  getCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+      }, (error) => {
+        console.error('Error getting location', error);
+      });
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }
 
   toggleSearch() {
     this.showSearchForm = !this.showSearchForm;
